fae8e247f84064b95f216937161076e079bc6099
Paola <Paola@DESKTOP-U6UR9H7>
2021-06-16 11:50:42 -0300

    Paola Flores: Primer Commit del Trabajo Final de Programación Visual.

---
+++ TrabajoFinal-EJPK21/src/main/java/ar/edu/unju/fi/TPFinal/TrabajoFinalEjpk21Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.TPFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalEjpk21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalEjpk21Application.class, args);
+	}
+
+}
